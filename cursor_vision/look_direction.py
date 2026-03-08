1257d35f9f00d30bba70cf2d961caf86fd450396
Kody Graham <KGraham.cs@gmail.com>
2026-03-07 23:13:07 -0600

    added all of the remaining expected methods for look direction, may need a few more but i think this is a really good base

--- cursor_vision/look_direction.py
+++ cursor_vision/look_direction.py
@@ -1,8 +1,19 @@
 import cv2
 
 class LookDirection:
-    right_iris = [469, 470, 471,472]
-    left_iris = [474, 475, 476,477]
+    right_iris = [468,469, 470, 471,472]
+    left_iris = [473, 474, 475, 476,477]
+
+    left_eye_outside = 33
+    left_eye_inside = 133
+    right_eye_outside = 263
+    right_eye_inside = 262
+
+    left_eyebrow = [70, 63, 105, 66, 107]
+    right_eyebrow = [336, 296, 334, 293,300]
+
+    deadzone =.01
+    divide_by_zero = .000001
 
     def __init__(self):
         self.dot_speed = 18
@@ -12,11 +23,22 @@ class LookDirection:
     def to_pixels(self, landmark, frame_width: int, frame_height: int) -> np.ndarray:
         return np.array([landmark.x, frame_width, landmark.y * frame_height], dtype=np.float32)
 
+    def get_current_cursor_position:
+    def current_look_point(self):
+    def eye_data(self):
+
     def eye_center(self, landmarks, landmarkID, frame_width: int, frame_height: int) -> np.ndarray:
 
         surrounding_landmark_locations = [self.to_pixels(landmarks[i], frame_width, frame_height) for i in landmarkID]
 
         return mean(np.stack(surrounding_landmark_locations, axis=0), axis=0)
+
+    def landmark_features(self):
+
+    def neutral_eye_center(self, landmarks, landmarkID, frame_width: int, frame_height: int) -> np.ndarray:
+
+    def clear_neutral_eye_center(self):
+
 
     def draw(self, frame, face_landmarks) -> bool:
 
